a9fa04be4f24c00badec8d2658155eb658eabb84
Mridul Jain <mriduljain202003@gmail.com>
2025-01-19 12:48:38 +0530

    Adding express.json()

--- src/app.js
+++ src/app.js
@@ -5,21 +5,27 @@ const User = require('./models/user.js');
 const app = express();
 const port = 3000;
 
+// now we can use json object to convert it to js object
+app.use(express.json());
+
 
 app.post("/signup", async (req,res) => {
-    const userObj = {
-        firstName : "Rohit",
-        lastName : "Sharma",
-        emailId : "rohitsharma202003@gmail.com",
-        password : "9093931902",  
-    }; 
-// createing a new instance of userModel
-    const user = new User(userObj);
 
-   await user.save();
+/*console.log(req.body); 
+     const { firstName , lastName , email , password } = req.body;
+     const userObj = {
+        firstName : firstName,
+        lastName : lastName,
+        email : email,
+        password : password,
+     };
+*/
+// createing a new instance of userModel
+    const user = new User(req.body);
+    await user.save();
     res.send("User created");
 
-})
+});
 
 
 
