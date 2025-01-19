bf2718421ca4967e5722726d120932a4bb8891d9
Mridul Jain <mriduljain202003@gmail.com>
2025-01-19 16:15:50 +0530

    Diving into Apis

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const port = 3000;
 // now we can use json object to convert it to js object
 app.use(express.json());
 
-
+//add a user
 app.post("/signup", async (req,res) => {
 
 /*console.log(req.body); 
@@ -26,8 +26,69 @@ app.post("/signup", async (req,res) => {
     res.send("User created");
 
 });
+//delete a user
+app.delete("/user", async (req, res) => {
+    const userId = req.body?.userId;
+    try{
+        const user = await User.findByIdAndDelete({
+            _id : userId
+        });
+        res.send("User deleted Successfully");
+    }catch(err){
+        res.status(400).send("Something went wrong");
+    }
+     
 
 
+});
+//get user by email
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const user = await User.findOne({
+        emailId : userEmail });
+
+        if(user.length === 0){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(user);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+    
+});
+//Fetch api
+app.get("/feed" , async (req,res) => {
+    try{
+        const users = await User.find({});
+
+        if(users.length === 0){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
+//update user
+app.patch("/user", async (req,res) => {
+    const data = req.body;
+    const userId = req.body.userId;
+    try{
+        const user = await User.findByIdAndUpdate({
+            _id : userId
+        },data, { returnDocument : "before"});
+        console.log(user);
+        res.send("User updated");
+    } catch (err) {
+        res.status(400).send("Something went wrong");
+    }
+});
 
 
 // Connect to the database first, then start the server
